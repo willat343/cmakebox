79c194ff38c138b324ff8b02938cd5d72563dcc4
William Talbot <william.talbot97@gmail.com>
2025-07-21 11:13:19 +0200

    fixed erroneous warning in import_Eigen3()

--- cmake/Dependencies/Eigen3.cmake
+++ cmake/Dependencies/Eigen3.cmake
@@ -30,7 +30,7 @@ function(import_Eigen3)
     endforeach()
 
     if (NOT DEPENDENCY_USE_SYSTEM AND DEPENDENCY_VERSION VERSION_LESS "3.5.0")
-        message(AUTHOR_WARNING "Fetching ${DEPENDENCY} ${DEPENDENCY_VERSION} which defines an uninstall target that "
+        message(AUTHOR_WARNING "Fetching Eigen3 ${DEPENDENCY_VERSION} which defines an uninstall target that "
             "may interfere with other projects. This is fixed from VERSION 3.5.0 "
             "(Issue: https://gitlab.com/libeigen/eigen/-/issues/1892, "
             "Merge Request: https://gitlab.com/libeigen/eigen/-/merge_requests/1885).")
