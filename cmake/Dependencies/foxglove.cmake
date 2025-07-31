6d6f499c936a821bbbbd258921a485a13059d891
William Talbot <william.talbot97@gmail.com>
2025-07-31 16:54:04 +0200

    added foxglove dependency

---
+++ cmake/Dependencies/foxglove.cmake
@@ -0,0 +1,98 @@
+# Import foxglove as:
+#   import_foxglove(
+#        VERSION <STRING:version>
+#        [METHOD <STRING:FETCH_GIT|FETCH_URL>]
+#        [USE_STATIC_LIBRARY]
+#   )
+#
+# Default METHOD is URL.
+#
+# Swap from shared library to static library with USE_STATIC_LIBRARY option.
+#
+# Link to foxglove target with:
+#   target_link_libraries(<target> <INTERFACE|PUBLIC|PRIVATE> foxglove::foxglove)
+function(import_foxglove)
+    set(OPTIONS
+        USE_STATIC_LIBRARY
+    )
+    set(SINGLE_VALUE_ARGS
+        VERSION
+        METHOD
+    )
+    set(MULTI_VALUE_ARGS)
+    cmake_parse_arguments(
+        DEPENDENCY
+        "${OPTIONS}"
+        "${SINGLE_VALUE_ARGS}"
+        "${MULTI_VALUE_ARGS}"
+        ${ARGN}
+    )
+
+    if (NOT DEPENDENCY_METHOD)
+        set(DEPENDENCY_METHOD "FETCH_URL")
+    endif()
+
+    set(URL_HASH)
+    if (DEPENDENCY_METHOD STREQUAL "FETCH_URL")
+        if (DEPENDENCY_VERSION VERSION_EQUAL "0.7.1")
+            set(URL_HASH "SHA256=2f942f3cefe27ee3de9bdb67df28a84042327103fe9733bd84b123de424a17a2")
+        elseif(DEPENDENCY_VERSION VERSION_EQUAL "0.8.0")
+            set(URL_HASH "SHA256=e539303b0db420638ae7d627a3d85df59a525078960a713c736b2728bba59e3c")
+        elseif(DEPENDENCY_VERSION VERSION_EQUAL "0.8.1")
+            set(URL_HASH "SHA256=d7a403435f543a6c5a8fbe431a72924152fc7f791c8e8e3e6cce4b2fd82ae467")
+        elseif(DEPENDENCY_VERSION VERSION_EQUAL "0.9.0")
+            set(URL_HASH "SHA256=48ea17c348583154d7c1934c9f7e82f57067e2afa76b586e1f7ac14cc6b6c1d0")
+        elseif(DEPENDENCY_VERSION VERSION_EQUAL "0.9.1")
+            set(URL_HASH "SHA256=7a9463270fac3cdf99ca5d41e597cae41e2c8b59a48b9eafec7a9ad6a70b11b1")
+        elseif(DEPENDENCY_VERSION VERSION_EQUAL "0.10.0")
+            set(URL_HASH "SHA256=209d34a8703d44a129c559493e1a3fb215888b4d5973622750ac28b1c345946c")
+        elseif(DEPENDENCY_VERSION VERSION_EQUAL "0.10.1")
+            set(URL_HASH "SHA256=099b022d1e911fbb7af1b1a85ced5234b10c7de753b24d402afcb595bc3add01")
+        else()
+            message(AUTHOR_WARNING "import_foxglove: Could not get URL_HASH for METHOD ${DEPENDENCY_METHOD}. "
+                "Either the specified VERSION ${DEPENDENCY_VERSION} was incorrect or import_foxglove's URL_HASH"
+                "database needs to be updated to contain the hash of this VERSION.")
+        endif()
+    endif()
+    
+    import_dependency(
+        foxglove
+        TARGET foxglove::foxglove
+        METHOD ${DEPENDENCY_METHOD}
+        FIND_PACKAGE_VERSION ${DEPENDENCY_VERSION}
+        GIT_REPOSITORY https://github.com/foxglove/foxglove-sdk.git
+        GIT_TAG sdk/v${DEPENDENCY_VERSION}
+        SOURCE_SUBDIR cpp
+        URL https://github.com/foxglove/foxglove-sdk/releases/download/sdk/v${DEPENDENCY_VERSION}/foxglove-v${DEPENDENCY_VERSION}-cpp-x86_64-unknown-linux-gnu.zip
+        URL_HASH ${URL_HASH}
+    )
+    if (NOT foxglove_SOURCE_DIR)
+        FetchContent_GetProperties(foxglove SOURCE_DIR foxglove_SOURCE_DIR)
+    endif()
+
+    if (NOT TARGET foxglove::foxglove)  
+        set(foxglove_TARGET foxglove_cpp_shared)
+        if (DEPENDENCY_USE_STATIC_LIBRARY)
+            set(foxglove_TARGET foxglove_cpp_static)
+        endif()
+        if (DEPENDENCY_METHOD STREQUAL "FETCH_GIT")
+            add_library(foxglove::foxglove ALIAS ${foxglove_TARGET})
+        elseif (DEPENDENCY_METHOD STREQUAL "FETCH_URL")
+            set(foxglove_LIBRARY_TYPE SHARED)
+            if (DEPENDENCY_USE_STATIC_LIBRARY)
+                set(foxglove_LIBRARY_TYPE STATIC)
+            endif()
+            set(foxglove_LIBRARY_FILE libfoxglove.so)
+            if (DEPENDENCY_USE_STATIC_LIBRARY)
+                set(foxglove_LIBRARY_FILE libfoxglove.a)
+            endif()
+            add_library(${foxglove_TARGET} ${foxglove_LIBRARY_TYPE} IMPORTED)
+            set_target_properties(${foxglove_TARGET} PROPERTIES
+                IMPORTED_LOCATION "${foxglove_SOURCE_DIR}/lib/${foxglove_LIBRARY_FILE}"
+                INTERFACE_INCLUDE_DIRECTORIES "${foxglove_SOURCE_DIR}/include"
+            )
+            add_library(foxglove::foxglove ALIAS ${foxglove_TARGET})
+        endif()
+    endif()
+
+endfunction()
